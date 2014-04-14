520d4abc4c6eac206b9bb844a564212ad10d4f51
Alex Scarborough <alex@gameclay.com>
2014-04-14 10:59:19 -0700

    First try at snowflake

---
+++ snowflake/recipes/default.rb
@@ -0,0 +1,71 @@
+user "snowflake" do
+  comment "snowflake Administrator"
+  supports :manage_home => false
+  system true
+end
+
+group "snowflake" do
+end
+
+directory "/usr/share/snowflake" do
+  owner "root"
+  group "root"
+  mode 0755
+  action :create
+end
+
+directory "/usr/share/snowflake/libs" do
+  owner "root"
+  group "root"
+  mode 0755
+  action :create
+end
+
+src_filepath  = "#{Chef::Config['file_cache_path'] || '/tmp'}/snowflake-package-dist.tar.gz"
+remote_file src_filepath do
+  source node[:snowflake][:url]
+  checksum node[:snowflake][:checksum]
+  backup false
+end
+
+bash "untar snowflake" do
+  user "root"
+  cwd "/tmp"
+  code %(tar zxf #{src_filepath})
+end
+
+bash "copy snowflake root" do
+  user "root"
+  cwd "/tmp"
+  code %(cp -r snowflake-package-dist/* /usr/share/snowflake)
+end
+
+directory "/etc/snowflake" do
+  owner "root"
+  group "root"
+  mode 0755
+  action :create
+end
+
+
+directory "/etc/snowflake" do
+  owner "root"
+  group "root"
+  mode 0755
+  action :create
+end
+
+directory "/var/log/snowflake" do
+  owner "snowflake"
+  group "snowflake"
+  mode 0755
+  action :create
+end
+
+template "/etc/init/snowflake.conf" do
+  source "snowflaked.conf.erb"
+  owner "root"
+  group "root"
+end
+
+include_recipe "snowflake::update_cluster"
---
+++ snowflake/recipes/update_cluster.rb
@@ -0,0 +1,13 @@
+service "snowflake" do
+  provider Chef::Provider::Service::Upstart
+  action :enable
+  supports :status => true, :restart => true
+end
+
+template "/etc/snowflake/snowflake.scala" do
+  source "snowflake.scala.erb"
+  mode "0644"
+  owner "root"
+  group "root"
+  notifies :restart, resources(:service => "snowflake")
+end
