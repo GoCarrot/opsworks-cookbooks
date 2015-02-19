750d3e0c0233cb4607f4b82b9f19aa993e52c493
Alex Scarborough <alex@gameclay.com>
2015-02-19 14:55:15 -0800

    More brutal and direct snowflake log clearing

--- snowflake/recipes/clear_logs.rb
+++ snowflake/recipes/clear_logs.rb
@@ -1,7 +1,5 @@
-include_recipe "snowflake::update_cluster"
-
-# Delete our log file (gets kinda big) and restart snowflake
-file "/var/log/snowflake/snowflake.log" do
-  action :delete
-  notifies :restart, 'service[snowflake]', :delayed
+bash "delete snowflake log" do
+  user "root"
+  cwd "/var/log/snowflake"
+  code %(rm -fr snowflake.log && restart snowflake)
 end
