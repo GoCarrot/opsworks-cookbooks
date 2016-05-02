a3d302f5b3caeb2e2f0da0dc4e4e100b885ce0bd
Alex Scarborough <alex@gameclay.com>
2016-05-02 10:12:37 -0700

    I think this will only restart the one sidekiq we care about

--- sidekiq/recipes/restart.rb
+++ sidekiq/recipes/restart.rb
@@ -4,6 +4,7 @@
 #
 
 node[:deploy].each do |application, deploy|
+  next if !deploy[:scm]
   execute "restart-sidekiq" do
     command %Q{
       echo "sleep 20 && monit -g sidekiq_#{application} restart all" | at now
