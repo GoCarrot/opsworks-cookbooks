77c26bd81e73937553710710d6ce49c88483ea66
Alex Scarborough <alex@teak.io>
2018-02-17 09:58:46 -0800

    Add ffmpeg installation.

---
+++ ffmpeg/recipes/install.rb
@@ -0,0 +1,18 @@
+src_filepath  = "#{Chef::Config['file_cache_path'] || '/tmp'}/ffmpeg.tar.gz"
+remote_file src_filepath do
+  source node[:ffmpeg][:url]
+  checksum node[:ffmpeg][:checksum]
+  backup false
+end
+
+bash "untar ffmpeg" do
+  user "root"
+  cwd "#{Chef::Config['file_cache_path'] || '/tmp'}"
+  code %(tar -xf #{src_filepath} --strip 1)
+end
+
+bash "copy ffmpgeg root" do
+  user "root"
+  cwd "#{Chef::Config['file_cache_path'] || '/tmp'}"
+  code %(cp ffmpeg /usr/bin/ffmpeg)
+end
