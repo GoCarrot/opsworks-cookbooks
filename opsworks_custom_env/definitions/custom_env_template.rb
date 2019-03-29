a4e1c86276b959563206dad1094a40d929f89efa
Alex Scarborough <alex@teak.io>
2019-03-29 10:04:44 -0700

    Don't run opsworks_custom_env if no custom env is present.

--- opsworks_custom_env/definitions/custom_env_template.rb
+++ opsworks_custom_env/definitions/custom_env_template.rb
@@ -2,11 +2,11 @@
 #   application (application name)
 #   deploy (hash of deploy attributes)
 #   env (hash of custom environment settings)
-# 
+#
 # Notifies a "restart Rails app <name>" resource.
 
 define :custom_env_template do
-  
+
   template "#{params[:deploy][:deploy_to]}/shared/config/application.yml" do
     source "application.yml.erb"
     owner params[:deploy][:user]
@@ -15,7 +15,7 @@ define :custom_env_template do
     variables :env => params[:env]
     notifies :run, resources(:execute => "restart Rails app #{params[:application]}")
 
-    only_if { File.exists?("#{params[:deploy][:deploy_to]}/shared/config") }
+    only_if { File.exists?("#{params[:deploy][:deploy_to]}/shared/config") && params[:env].present? }
   end
-  
+
 end
