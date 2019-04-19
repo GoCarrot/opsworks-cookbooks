4d6c55a85481ab5a311452cf37efaf1cd24e67a9
Alex Scarborough <alex@teak.io>
2019-04-19 09:38:05 -0700

    Suppress restarting unicorns unless the instance is on a rails-app layer
    Fixes #1

--- rails/recipes/configure.rb
+++ rails/recipes/configure.rb
@@ -7,6 +7,11 @@ node[:deploy].each do |application, deploy|
     cwd deploy[:current_path]
     command node[:opsworks][:rails_stack][:restart_command]
     action :nothing
+
+    # Only restart rails apps on rails-app layer. This is to prevent unicorns
+    # from running on sidekiq worker servers, unless they are also web tier
+    # servers.
+    only_if { node[:opsworks][:instance][:layers].include?('rails-app') }
   end
 
   node.default[:deploy][application][:database][:adapter] = OpsWorks::RailsConfiguration.determine_database_adapter(application, node[:deploy][application], "#{node[:deploy][application][:deploy_to]}/current", :force => node[:force_database_adapter_detection])
